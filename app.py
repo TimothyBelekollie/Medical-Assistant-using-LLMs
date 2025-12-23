8cb0a17f3c7d0d8e5a753b2fcab551176ea0b479
TimothyBelekollie <belekollietimothy2@gmail.com>
2025-12-23 20:48:19 +0200

    making changes to the appy.py for the project to run and also making changes to the helper.py to fix depricated imports

--- app.py
+++ app.py
@@ -66,5 +66,5 @@ def chat():
 
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 8080, debug= True)
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5001, debug=True)
--- src/helper.py
+++ src/helper.py
@@ -1,4 +1,5 @@
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+#from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from typing import List
